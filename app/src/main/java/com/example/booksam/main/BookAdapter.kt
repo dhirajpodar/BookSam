da39cc4c3b562502953a27550d705e6ae560cfcb
Dhiraj Poddar <dhiraj.poddar@citytech.global>
2020-10-01 19:07:25 +0545

    first commit

--- app/src/main/java/com/example/booksam/main/BookAdapter.kt
+++ app/src/main/java/com/example/booksam/main/BookAdapter.kt
@@ -1,10 +1,10 @@
 package com.example.booksam.main
 
 import android.content.Context
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.repo.Book
 import com.example.booksam.R
@@ -13,6 +13,7 @@ import kotlinx.android.synthetic.main.custom_book_view.view.*
 class BookAdapter(private val optionSelectedListener: OptionSelectedListener) :
     RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
 
+    private lateinit var context: Context
     private var books = emptyList<Book>()
 
 
@@ -22,7 +23,7 @@ class BookAdapter(private val optionSelectedListener: OptionSelectedListener) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
-
+        this.context = parent.context
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.custom_book_view, parent, false)
         return BookViewHolder(view)
@@ -33,6 +34,11 @@ class BookAdapter(private val optionSelectedListener: OptionSelectedListener) :
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
         holder.itemView.tv_title.text = books.get(position).title
         holder.itemView.tv_author.text = books.get(position).author
+        if (books.get(position).author.equals("Gaur Gopal Das")) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                holder.itemView.iv_book_cover.setImageDrawable(context.getDrawable(R.drawable.las))
+            }
+        }
 
         holder.itemView.tv_summary.setOnClickListener {
             optionSelectedListener.optionPicked("SUMMARY", holder.adapterPosition)
